8bcbdc84661650af454ce51727ccea6339e40999
Pavel Aseev <manjaro@localhost.localdomain>
2020-05-22 20:59:31 +0000

    Initial cpp code

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* main.cpp
+ * This file is part of EchoVox Raspberry Pi project.
+ * 
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+/**
+ * Program entry point
+ */
+int main()
+{
+    cout << "Starting EchoVox..." << endl;
+}
